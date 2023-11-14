66b207e68ecbbcd7ba955b9f904155ae316946f9
Hugo Caroumbin | "RatonFuté <hcaroumbin@gaming.tech>
2023-11-14 12:58:21 +0100

    mouse pos

--- Client/main.cpp
+++ Client/main.cpp
@@ -2,12 +2,23 @@
 
 // Client Window Procedure
 LRESULT CALLBACK ClientWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+
+    int x, y = 0;
+    POINT mousePos;
+    if (GetCursorPos(&mousePos))
+    {
+        x = mousePos.x;
+        y = mousePos.y;
+    }
+
+
     switch (message) {
     case WM_CREATE:
     {
         // Set up client (create socket, connect to the server, etc.)
         WSADATA wsaData;
         if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+            MessageBox(hWnd, L"WSAStartup failed", L"ERROR", 0);
             std::cerr << "WSAStartup failed." << std::endl;
             PostQuitMessage(1);
             return -1;
@@ -16,6 +27,7 @@ LRESULT CALLBACK ClientWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lP
 
         SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
         if (clientSocket == INVALID_SOCKET) {
+            MessageBox(hWnd, L"Error creating socket", L"ERROR", 0);
             std::cerr << "Error creating socket: " << WSAGetLastError() << std::endl;
             PostQuitMessage(1);
             return -1;
@@ -26,15 +38,19 @@ LRESULT CALLBACK ClientWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lP
         sockaddr_in serverAddress;
         if (InetPton(AF_INET, ipAddressText, &(serverAddress.sin_addr)) == 1) {
             // Conversion successful
+
             std::cout << "Binary representation: " << serverAddress.sin_addr.S_un.S_addr << std::endl;
         }
         else {
             // Conversion failed
+            MessageBox(hWnd, L"InetPton failed. Error code", L"ERROR", 0);
+
             std::cerr << "InetPton failed. Error code: " << WSAGetLastError() << std::endl;
         }
 
         if (connect(clientSocket, reinterpret_cast<SOCKADDR*>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR) {
-            std::cerr << "Error connecting to server: " << WSAGetLastError() << std::endl;
+            MessageBox(hWnd, L"Error connecting to server" , L"ERROR", 0);
+            std::cerr << "Error connecting to server" << WSAGetLastError() << std::endl;
             closesocket(clientSocket);
             PostQuitMessage(1);
             return -1;
@@ -86,7 +102,7 @@ int  WinMain(
     while (GetMessage(&msg, nullptr, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
-    }
 
-    return 0;
+        return 0;
+    }
 }
--- Client/pch.h
+++ Client/pch.h
@@ -5,6 +5,7 @@
 #include <memory>
 #include <vector>
 #include <Windows.h>
+#include <string> 
 
 
 
