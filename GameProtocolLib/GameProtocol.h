d1d1b0150deade54d85918f2505d108661aa85e4
Tom LE GROS <freezekaz.dement@gmail.com>
2023-11-15 10:08:25 +0100

    :wrench: Add lib + nlohmann

---
+++ GameProtocolLib/GameProtocol.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "GameProtocol.h"
+#include <windows.data.json.h>
+#include <unordered_map>
+#include "../nlohmann/json.hpp"
+
+static class GameProtocol
+{
+private:
+public:
+	GameProtocol();
+	~GameProtocol();
+
+	//void handleMessages(const std::string& message, std::unordered_map<SOCKET>& clients);
+
+
+
+};
