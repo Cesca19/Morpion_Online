943cee76c149ade834c9a3656ba94c8562a6ecda
Tom LE GROS <freezekaz.dement@gmail.com>
2023-11-16 16:27:30 +0100

    :star: Message / Protocol lib done

---
+++ GameProtocolLib/GameProtocol.cpp
@@ -0,0 +1,157 @@
+#include "GameProtocol.h"
+
+namespace Protocol
+{
+
+
+	GameProtocol::GameProtocol(){}
+	GameProtocol::~GameProtocol(){}
+
+
+	GameProtocol::GameStateMessage GameProtocol::handleGameStateMessage(const nlohmann::json& message)
+	{
+		GameProtocol::GameStateMessage msg{};
+		msg = message["type"].get<std::string>() == "GAME" ? GameProtocol::processGameStateMessage(message) : msg;
+		return msg;
+	}	
+	GameProtocol::MoveMessage GameProtocol::handleMoveMessage(const nlohmann::json& message)
+	{
+		GameProtocol::MoveMessage msg{};
+		msg = message["type"].get<std::string>() == "MOVE" ? GameProtocol::processMoveMessage(message) : msg;
+		return msg;
+	}
+	GameProtocol::NewClientMessage GameProtocol::handleNewClientMessage(const nlohmann::json& message)
+	{
+		GameProtocol::NewClientMessage msg{};
+		msg = message["type"].get<std::string>() == "NEW_CLIENT" ? GameProtocol::processNewClientMessage(message) : msg;
+		return msg;
+	}
+	GameProtocol::ClientIDMessage GameProtocol::handleClientIdMessage(const nlohmann::json& message)
+	{
+		GameProtocol::ClientIDMessage msg{};
+		msg = message["type"].get<std::string>() == "CLIENT_ID" ? GameProtocol::processClientIdMessage(message) : msg;
+		return msg;
+	}
+
+	GameProtocol::GameStateMessage GameProtocol::processGameStateMessage(const nlohmann::json& message)
+	{
+		GameProtocol::GameStateMessage msg;
+
+		auto board = message["game"]["board"].get<std::vector<std::vector<int>>>();
+		int rows = board.size();
+		int col = board[0].size();
+		msg.board = new int*[rows];
+
+		for (int i = 0; i < rows; ++i) {
+			msg.board[i] = new int[col];
+			std::copy(board[i].begin(), board[i].end(), msg.board[i]);
+		}
+		
+		msg.currentPlayer = message["game"]["currentPlayer"].get<int>();
+		msg.turnNumber = message["game"]["turnNumber"].get<int>();
+		msg.winner = message["game"]["winner"].get<int>();
+
+		return msg;
+	}	
+	GameProtocol::MoveMessage GameProtocol::processMoveMessage(const nlohmann::json& message)
+	{
+		GameProtocol::MoveMessage msg;
+
+		msg.id = message["data"]["playerID"].get<int>();
+		msg.posX = message["data"]["posX"].get<int>();
+		msg.posY = message["data"]["posY"].get<int>();
+
+		return msg;
+	}
+	GameProtocol::NewClientMessage GameProtocol::processNewClientMessage(const nlohmann::json& message)
+	{
+		GameProtocol::NewClientMessage msg;
+
+		msg.id = message["data"]["id"].get<int>();
+		msg.name = message["data"]["name"].get<std::string>();
+
+		return msg;
+	}
+	GameProtocol::ClientIDMessage GameProtocol::processClientIdMessage(const nlohmann::json& message)
+	{
+		GameProtocol::ClientIDMessage msg;
+
+		msg.id = message["data"]["id"].get<int>();
+		msg.playerState = message["data"]["playerState"].get<int>();
+
+		return msg;
+	}
+
+	std::string GameProtocol::createGameStateMessage(int** gameBoard, int turnNumber, int winner, int currentPlayer)
+	{
+
+		std::vector<std::vector<int>> boardVector;
+		int rows = std::extent<decltype(gameBoard), 0>::value;
+		int cols = std::extent<decltype(gameBoard), 1>::value;
+		// Copy the contents of the array to the vector
+		for (int i = 0; i < rows; ++i) {
+			std::vector<int> row;
+			for (int j = 0; j < cols; ++j) {
+				row.push_back(gameBoard[i][j]);
+			}
+			boardVector.push_back(row);
+		}
+		//boardVector just for the message 
+
+		nlohmann::json message;
+
+		message = {
+		 {"type", "GAME"},
+		 {"game", {
+			 {"board", boardVector},
+			 {"currentPlayer", currentPlayer},
+			 {"turn", turnNumber},
+			 {"winner", winner}
+		 }}
+		};
+
+		return message.dump();
+	}
+	std::string GameProtocol::createMoveMessage(int id, int posX, int poxY)
+	{
+		nlohmann::json message;
+
+		message = {
+		 {"type", "MOVE"},
+		 {"data", {
+			 {"id", id},
+			 {"posX", posX},
+			 {"posY", poxY}
+		 }}
+		};
+
+		return message.dump();
+;	}
+	std::string GameProtocol::createNewClientMessage(int id, std::string name)
+	{
+		nlohmann::json message;
+
+		message = {
+		 {"type", "NEW_CLIENT"},
+		 {"data", {
+			 {"id", id},
+			 {"name", name}
+		 }}
+		};
+
+		return message.dump();
+	}
+	std::string GameProtocol::createClientIdMessage(int playerId, int playerState)
+	{
+		nlohmann::json message;
+		message = {
+		 {"type", "CLIENT_ID"},
+		 {"data", {
+			 {"playerID", playerId},
+			 {"playerState", playerState}
+		 }}
+		};
+		return message.dump();
+	}
+
+}
--- GameProtocolLib/GameProtocol.h
+++ GameProtocolLib/GameProtocol.h
@@ -4,15 +4,69 @@
 #include <unordered_map>
 #include "../nlohmann/json.hpp"
 
-class GameProtocol
-{
-private:
-public:
-	GameProtocol();
-	~GameProtocol();
 
-	void handleMessages(const std::string& message, SOCKET client);
-	nlohmann::json receiveJsonMessage(SOCKET socket);
-	void sendJsonMessage(SOCKET socket, const nlohmann::json& message);
-		 
-};
+namespace Protocol
+{
+	static class GameProtocol
+	{
+	public:
+
+		enum class MessageType {
+			GAME_STATE,
+			MOVE,
+			NEW_CLIENT,
+		};
+		
+		struct GameStateMessage
+		{
+			int** board = NULL;
+			int currentPlayer = NULL;
+			int turnNumber = NULL;
+			int winner = NULL;
+		};
+
+		struct MoveMessage
+		{
+			int id = NULL;
+			int posX = NULL;
+			int posY = NULL;
+		};
+
+		struct NewClientMessage
+		{
+			int id = NULL;
+			std::string name = NULL;
+		};
+
+		struct ClientIDMessage
+		{
+			int id = NULL;
+			int playerState = NULL;
+		};
+
+	public:
+		
+
+		GameProtocol();
+		~GameProtocol();
+
+		GameStateMessage handleGameStateMessage(const nlohmann::json& message); 
+		MoveMessage handleMoveMessage(const nlohmann::json& message);
+		NewClientMessage handleNewClientMessage(const nlohmann::json& message);
+		ClientIDMessage handleClientIdMessage(const nlohmann::json& message);
+
+
+	public:
+
+		static GameStateMessage processGameStateMessage(const nlohmann::json& message); //client receives the gamestete from the server
+		static MoveMessage processMoveMessage(const nlohmann::json& message); //server receives the move from client
+		static NewClientMessage processNewClientMessage(const nlohmann::json& message); // client receives the new clien that logged in and add it to the vector 
+		static ClientIDMessage processClientIdMessage(const nlohmann::json& message);  //client receives his own id from the server
+
+		std::string createGameStateMessage(int** gameBoard, int turnNumber, int winner, int currentPlayer); //server creates a GameState message using his own value 
+		std::string createMoveMessage(int id, int posX, int poxY); //player sends the move to the server
+		std::string createNewClientMessage(int id, std::string name);// server create a message for clients to introduce new client
+		std::string createClientIdMessage(int playerId, int playerState); //server creates a message to send a clien'ts ID to this client
+
+	};
+}
